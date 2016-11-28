9346d38fc87aff5fdd37a67a8ec21633bd33bc66
Gyssim <simon.gysin@students.fhnw.ch>
2016-11-28 13:30:49 +0100

    test commit redo

--- src/ch/imedias/rscc/SupportAddress.java
+++ src/ch/imedias/rscc/SupportAddress.java
@@ -19,7 +19,7 @@ public class SupportAddress implements Serializable {
      * creates a new SupportAddress
      *
      * @param description the description
-     * @param address the addresss
+     * @param address the address
      * @param encrypted if the connection is encrypted
      */
     public SupportAddress(
