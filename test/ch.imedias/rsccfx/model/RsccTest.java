d11222d09e5670d2bb9c9e3353dd19f533a39916
Gyssim <simon.gysin@students.fhnw.ch>
2016-11-28 16:09:40 +0100

    rscc test class added

---
+++ test/ch.imedias/rsccfx/model/RsccTest.java
@@ -0,0 +1,16 @@
+package ch.imedias.rsccfx.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 28.11.16.
+ */
+public class RsccTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
