da51d75750689cc2553742f5a97ee644e8410cc6
Patrick Wigger <patrick.wigger@students.fhnw.ch>
2016-11-30 11:23:16 +0100

    initial homeview

--- src/ch/imedias/rsccfx/RsccApp.java
+++ src/ch/imedias/rsccfx/RsccApp.java
@@ -1,6 +1,7 @@
 package ch.imedias.rsccfx;
 
 import ch.imedias.rsccfx.model.Rscc;
+import ch.imedias.rsccfx.view.RsccHomeView;
 import ch.imedias.rsccfx.view.RsccPresenter;
 import ch.imedias.rsccfx.view.RsccView;
 import javafx.application.Application;
@@ -17,12 +18,13 @@ public class RsccApp extends Application {
   @Override
   public void start(Stage stage) {
     Rscc model = new Rscc();
-    RsccView view = new RsccView(model);
+    RsccHomeView view = new RsccHomeView(model);
 
     // Must set the scene before creating the presenter that uses
     // the scene to listen for the focus change
     Scene scene = new Scene(view);
-
+    String stylesheet = getClass().getClassLoader().getResource("HomeStyle.css").toExternalForm();
+    scene.getStylesheets().add(stylesheet);
     RsccPresenter presenter = new RsccPresenter(model, view);
 
     stage.setScene(scene);
---
+++ src/ch/imedias/rsccfx/view/RsccHomeView.java
@@ -0,0 +1,51 @@
+package ch.imedias.rsccfx.view;
+
+import ch.imedias.rsccfx.model.Rscc;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+/**
+ * Created by pwg on 30.11.16.
+ */
+public class RsccHomeView extends Parent {
+  private final Rscc model;
+  private Button requestSupportBtn;
+  private Button offerSupportBtn;
+  private HBox homeBox;
+
+  public RsccHomeView(Rscc model) {
+    this.model = model;
+
+    initFieldData();
+    layoutForm();
+    bindFieldsToModel();
+  }
+
+  private void initFieldData() {
+    requestSupportBtn = new Button();
+    requestSupportBtn.textProperty().setValue("Test"); //TODO: replace Text, multilangual
+    offerSupportBtn = new Button();
+    offerSupportBtn.textProperty().setValue("Test2"); // TODO: replace Text, multilangual
+    requestSupportBtn.setId("HomeNavigationBtn");
+    offerSupportBtn.setId("HomeNavigationBtn");
+    homeBox = new HBox();
+
+  }
+
+  private void bindFieldsToModel() {
+
+  }
+
+  private void layoutForm() {
+
+    homeBox.getChildren().add(requestSupportBtn);
+    homeBox.getChildren().add(offerSupportBtn);
+    this.getChildren().add(homeBox);
+
+
+
+  }
+
+
+}
