4b1d50c4bd376f40c2049e36e1a6526598ea3ce9
Jennifer Mueller <jennifer.mueller1@students.fhnw.ch>
2016-11-30 18:51:33 +0100

    Second solution of requestToken.

--- src/ch/imedias/rsccfx/model/Rscc.java
+++ src/ch/imedias/rsccfx/model/Rscc.java
@@ -13,31 +13,39 @@ public class Rscc {
   /** Requests a token from the key server. */
 
   public String requestTokenFromServer(int forwardingPort, String keyServerIp, int keyServerPort,boolean isCompressionEnabled){
-
     String currentPath = (new File("")).getAbsolutePath();
+    String workingPath = currentPath + "/resources/docker-build_p2p/";
+    System.out.println(workingPath);
+    File workingDirectory = new File(workingPath);
+
     StringBuilder command = new StringBuilder();
     // execute first command to import key server setup
 
-    command.append(currentPath + "/" + "port_share.sh" + " ");
+    command.append("port_share.sh" + " ");
     command.append("--p2p_server=" + keyServerIp + " ");
     command.append("--p2p_port=" + keyServerPort +" ");
     command.append("--compress=" + (isCompressionEnabled ? "yes" : "no") + " ");
     command.append(forwardingPort);
 
-    String key = executeTerminalCommand(command.toString());
+    System.out.println(command.toString());
 
-    System.out.println(key);
+    String key = executeTerminalCommand(workingDirectory, command.toString());
+
+
 
     return key;
   }
 
-  private String executeTerminalCommand(String command) {
+  private String executeTerminalCommand(File directory, String command) {
     StringBuffer output = new StringBuffer();
     Process p;
+    ProcessBuilder processBuilder = new ProcessBuilder();
 
     try {
       // Execute Command
-      p = Runtime.getRuntime().exec(command);
+      processBuilder.directory(directory);
+      processBuilder.command(command);
+      p = processBuilder.start();
       p.waitFor();
 
       // Gather output from Terminal
