959242ea6ec9b4d9edd7d8176cbe09e1ecc1dbc8
François Martin <Blizzard@chantals-iphone.edu.ds.fhnw.ch>
2016-11-30 17:15:08 +0100

    added method to execute linux commands from terminal

--- src/ch/imedias/rsccfx/model/Rscc.java
+++ src/ch/imedias/rsccfx/model/Rscc.java
@@ -1,12 +1,34 @@
 package ch.imedias.rsccfx.model;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Rscc {
   public Rscc() {
     //TODO required constructor(s)
 
   }
 
-  /**
-   *
-   */
+  private String executeTerminalCommand(String command) {
+    StringBuffer output = new StringBuffer();
+    Process p;
+
+    try {
+      // Execute Command
+      p = Runtime.getRuntime().exec(command);
+      p.waitFor();
+
+      // Gather output from Terminal
+      BufferedReader reader =
+          new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String line = "";
+      while ((line = reader.readLine()) != null) {
+        output.append(line + "\n");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return output.toString();
+  }
+
 }
