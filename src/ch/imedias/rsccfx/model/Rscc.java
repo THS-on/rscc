179dc5faf4cb55bdce6e5dc74f92122a9faf9915
Jennifer Mueller <jennifer.mueller1@students.fhnw.ch>
2016-11-30 17:15:00 +0100

    Added files for request token to docker-build_p2p.

--- src/ch/imedias/rsccfx/model/Rscc.java
+++ src/ch/imedias/rsccfx/model/Rscc.java
@@ -3,10 +3,22 @@ package ch.imedias.rsccfx.model;
 public class Rscc {
   public Rscc() {
     //TODO required constructor(s)
-
   }
 
-  /**
-   *
-   */
+
+  /** Requests a token from the key server. */
+
+  public String requestTokenFromServer(int forwardingPort, String keyServerIp, int keyServerPort,boolean isCompressionEnabled){
+    // generate key/create.key
+
+
+
+
+    // generate tempfile
+
+
+    //
+
+
+  }
 }
