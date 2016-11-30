d5cc44c76999a975b0e57b24b6251de1c57fac13
Jennifer Mueller <jennifer.mueller1@students.fhnw.ch>
2016-11-30 18:50:45 +0100

    First solution of requestToken.

--- src/ch/imedias/rsccfx/model/Rscc.java
+++ src/ch/imedias/rsccfx/model/Rscc.java
@@ -1,6 +1,7 @@
 package ch.imedias.rsccfx.model;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
 public class Rscc {
@@ -12,18 +13,24 @@ public class Rscc {
   /** Requests a token from the key server. */
 
   public String requestTokenFromServer(int forwardingPort, String keyServerIp, int keyServerPort,boolean isCompressionEnabled){
-    // generate key/create.key
 
+    String currentPath = (new File("")).getAbsolutePath();
+    StringBuilder command = new StringBuilder();
+    // execute first command to import key server setup
 
+    command.append(currentPath + "/" + "port_share.sh" + " ");
+    command.append("--p2p_server=" + keyServerIp + " ");
+    command.append("--p2p_port=" + keyServerPort +" ");
+    command.append("--compress=" + (isCompressionEnabled ? "yes" : "no") + " ");
+    command.append(forwardingPort);
 
+    String key = executeTerminalCommand(command.toString());
 
-    // generate tempfile
+    System.out.println(key);
 
-
-    //
-
-
+    return key;
   }
+
   private String executeTerminalCommand(String command) {
     StringBuffer output = new StringBuffer();
     Process p;
--- test/ch/imedias/rsccfx/model/RsccTest.java
+++ test/ch/imedias/rsccfx/model/RsccTest.java
@@ -1,15 +1,29 @@
 package ch.imedias.rsccfx.model;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
  *
  */
 public class RsccTest {
+    Rscc model;
   @Before
   public void setUp() throws Exception {
+    model = new Rscc();
+
+  }
+
+  @Test
+  public void requestTokenFromServer() throws Exception {
+
+    String key = model.requestTokenFromServer(4999, "86.119.39.89", 2201, true);
+
+    assertEquals(10,key.trim().length());
 
   }
 
