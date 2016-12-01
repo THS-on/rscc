076fa648e7048ad5bbe7ad117d16a51c6fc1f6d9
Gyssim <simon.gysin@students.fhnw.ch>
2016-12-01 09:03:06 +0100

    changed app

--- src/ch/imedias/rsccfx/RsccApp.java
+++ src/ch/imedias/rsccfx/RsccApp.java
@@ -3,6 +3,7 @@ package ch.imedias.rsccfx;
 import ch.imedias.rsccfx.model.Rscc;
 import ch.imedias.rsccfx.view.RsccPresenter;
 import ch.imedias.rsccfx.view.RsccView;
+import ch.imedias.rsccfx.view.ShowTokenView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -17,14 +18,16 @@ public class RsccApp extends Application {
   @Override
   public void start(Stage stage) {
     Rscc model = new Rscc();
-    RsccView view = new RsccView(model);
-
+    //RsccView view = new RsccView(model);
+    ShowTokenView view = new ShowTokenView(model);
     // Must set the scene before creating the presenter that uses
     // the scene to listen for the focus change
     Scene scene = new Scene(view);
 
     RsccPresenter presenter = new RsccPresenter(model, view);
 
+    stage.setWidth(500);
+    stage.setHeight(500);
     stage.setScene(scene);
     stage.setTitle(APP_NAME);
     stage.show();
