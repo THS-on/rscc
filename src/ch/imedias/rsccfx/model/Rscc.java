374fb83b857e68db126c5dc1a96041bd93fc154c
François Martin <Blizzard@Francoiss-MacBook-Air.local>
2016-12-02 13:54:31 +0100

    put executeTerminalCommand into SystemCommander class, wrote tests for requestTokenFromServer method

--- src/ch/imedias/rsccfx/model/Rscc.java
+++ src/ch/imedias/rsccfx/model/Rscc.java
@@ -1,8 +1,5 @@
 package ch.imedias.rsccfx.model;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Rscc {
   /**
    * Points to the "docker-build_p2p" folder inside resources, relative to the build path.
@@ -10,8 +7,10 @@ public class Rscc {
    */
   private static final String PATH_TO_RESOURCE_DOCKER = "resources/docker-build_p2p";
 
-  public Rscc() {
-    //TODO required constructor(s)
+  private final SystemCommander systemCommander;
+
+  public Rscc(SystemCommander systemCommander) {
+    this.systemCommander = systemCommander;
   }
 
   /**
@@ -26,7 +25,7 @@ public class Rscc {
     command.append("use.sh" + " ");
     command.append(keyServerIp + " ");
     command.append(keyServerHttpPort);
-    executeTerminalCommand(command.toString());
+    systemCommander.executeTerminalCommand(command.toString());
 
     command = new StringBuilder();
     // Execute port_share.sh and get a key as output
@@ -36,33 +35,9 @@ public class Rscc {
     command.append("--p2p_port=" + keyServerSshPort + " ");
     command.append("--compress=" + (isCompressionEnabled ? "yes" : "no") + " ");
     command.append(forwardingPort);
-
-    return executeTerminalCommand(command.toString());
+    return systemCommander.executeTerminalCommand(command.toString());
   }
 
-  // more detailed instructions concerning this command can be found here:
-  // https://www.cs.technik.fhnw.ch/confluence16/pages/viewpage.action?pageId=6929334
-  private String executeTerminalCommand(String command) {
-    Process process;
 
-    try {
-      StringBuffer output = new StringBuffer();
-      // Execute Command
-      process = Runtime.getRuntime().exec(command);
-      process.waitFor();
-      // read the output from the command
-      BufferedReader outputReader = new BufferedReader(new
-          InputStreamReader(process.getInputStream()));
-      String line = null;
-      while ((line = outputReader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-      outputReader.close();
-      return output.toString().trim();
-    } catch (Exception exception) {
-      exception.printStackTrace();
-    }
-    return "";
-  }
 
 }
--- test/ch/imedias/rsccfx/model/RsccTest.java
+++ test/ch/imedias/rsccfx/model/RsccTest.java
@@ -1,6 +1,9 @@
 package ch.imedias.rsccfx.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -14,22 +17,26 @@ public class RsccTest {
 
   @Before
   public void setUp() throws Exception {
-    model = new Rscc();
+
   }
 
   @Ignore
   @Test
   public void requestTokenFromServer() throws Exception {
+    final String terminalCommandUse = "bash resources/docker-build_p2p/use.sh 86.119.39.89 800";
+    final String terminalCommandPortShare =
+        "bash resources/docker-build_p2p/port_share.sh --p2p_server=86.119.39.89 --p2p_port=2201"
+            + " --compress=yes 4999";
+    SystemCommander mockCommander = mock(SystemCommander.class);
+    model = new Rscc(mockCommander);
+
+    when(mockCommander.executeTerminalCommand(terminalCommandPortShare)).thenReturn("6a2b9op6bq");
 
     String key = model.requestTokenFromServer(4999, "86.119.39.89", 2201, 800, true);
 
-    assertEquals(10, key.trim().length());
-
-  }
-
-  @Test
-  public void sampleTest() {
-
+    // actual test
+    verify(mockCommander).executeTerminalCommand(terminalCommandUse);
+    assertEquals("6a2b9op6bq",key);
   }
 
 }
