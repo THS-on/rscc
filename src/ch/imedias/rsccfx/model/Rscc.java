81281c412621a696f5bdfe8ab1aaf88cbd47b824
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2016-12-22 16:59:07 +0100

    added startVncServer method and completed connectToUser method

--- src/ch/imedias/rsccfx/model/Rscc.java
+++ src/ch/imedias/rsccfx/model/Rscc.java
@@ -1,46 +1,65 @@
 package ch.imedias.rsccfx.model;
 
 public class Rscc {
-  /**
-   * Points to the "docker-build_p2p" folder inside resources, relative to the build path.
-   * Important: Make sure to NOT include a / in the beginning or the end.
-   */
-  private static final String PATH_TO_RESOURCE_DOCKER = "resources/docker-build_p2p";
+    /**
+     * Points to the "docker-build_p2p" folder inside resources, relative to the build path.
+     * Important: Make sure to NOT include a / in the beginning or the end.
+     */
+    private static final String PATH_TO_RESOURCE_DOCKER = "resources/docker-build_p2p";
 
-  private final SystemCommander systemCommander;
+    private final SystemCommander systemCommander;
 
-  public Rscc(SystemCommander systemCommander) {
-    this.systemCommander = systemCommander;
-  }
+    public Rscc(SystemCommander systemCommander) {
+        this.systemCommander = systemCommander;
+    }
 
-  /**
-   * Requests a token from the key server.
-   */
-  public String requestTokenFromServer(int forwardingPort, String keyServerIp, int keyServerSshPort,
-                                       int keyServerHttpPort, boolean isCompressionEnabled) {
-    StringBuilder command = new StringBuilder();
+    /**
+     * Requests a token from the key server.
+     */
+    public String requestTokenFromServer(int forwardingPort, String keyServerIp, int keyServerSshPort,
+                                         int keyServerHttpPort, boolean isCompressionEnabled) {
+        StringBuilder command = new StringBuilder();
 
-    // First, setup the server with use.sh
-    command.append("bash" + " " + PATH_TO_RESOURCE_DOCKER + "/");
-    command.append("use.sh" + " ");
-    command.append(keyServerIp + " ");
-    command.append(keyServerHttpPort);
-    systemCommander.executeTerminalCommand(command.toString());
+        // First, setup the server with use.sh
+        command.append("bash" + " " + PATH_TO_RESOURCE_DOCKER + "/");
+        command.append("use.sh" + " ");
+        command.append(keyServerIp + " ");
+        command.append(keyServerHttpPort);
+        systemCommander.executeTerminalCommand(command.toString());
 
-    command = new StringBuilder();
-    // Execute port_share.sh and get a key as output
-    command.append("bash" + " " + PATH_TO_RESOURCE_DOCKER + "/");
-    command.append("start_vnc.sh" + " ");
-    command.append("--p2p_server=" + keyServerIp + " ");
-    command.append("--p2p_port=" + keyServerSshPort + " ");
-    command.append("--compress=" + (isCompressionEnabled ? "yes" : "no") + " ");
-    command.append(forwardingPort);
-    return systemCommander.executeTerminalCommand(command.toString());
-  }
+        command = new StringBuilder();
+        // Execute port_share.sh and get a key as output
+        command.append("bash" + " " + PATH_TO_RESOURCE_DOCKER + "/");
+        command.append("port_share.sh" + " ");
+        command.append("--p2p_server=" + keyServerIp + " ");
+        command.append("--p2p_port=" + keyServerSshPort + " ");
+        command.append("--compress=" + (isCompressionEnabled ? "yes" : "no") + " ");
+        command.append(forwardingPort);
+        return systemCommander.executeTerminalCommand(command.toString());
+    }
 
-  public void connectToUser(){
+    public void connectToUser(String keyServerIp, int keyServerSshPort, int keyServerHttpPort, String key) {
+        StringBuilder command = new StringBuilder();
+        command.append("bash" + " " + PATH_TO_RESOURCE_DOCKER + "/");
+        command.append("use.sh" + " ");
+        command.append(keyServerIp + " ");
+        command.append(keyServerHttpPort);
+        systemCommander.executeTerminalCommand(command.toString());
 
+        command = new StringBuilder();
 
-  }
+        // Execute start_vncviewer.sh and connect to the User
+        command.append("bash" + " " + PATH_TO_RESOURCE_DOCKER + "/");
+        command.append("start_vncviewer.sh " + key);
+        systemCommander.executeTerminalCommand(command.toString());
+    }
 
+    public void startVncServer(String key, int forwardingPort) {
+        StringBuilder command = new StringBuilder();
+        command.append("bash" + " " + PATH_TO_RESOURCE_DOCKER + "/");
+        command.append("start_vncserver.sh" + " ");
+        command.append("--vnc_port=" + forwardingPort + " ");
+        command.append("--key=" + key);
+        systemCommander.executeTerminalCommand(command.toString());
+    }
 }
