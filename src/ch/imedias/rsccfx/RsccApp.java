f942245768533f6263fd4697155ed020c51efaf5
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2017-03-08 23:36:38 +0100

    Fixed checkstyle error.

--- src/ch/imedias/rsccfx/RsccApp.java
+++ src/ch/imedias/rsccfx/RsccApp.java
@@ -3,8 +3,8 @@ package ch.imedias.rsccfx;
 import ch.imedias.rsccfx.model.Rscc;
 import ch.imedias.rsccfx.model.SystemCommander;
 
-import ch.imedias.rsccfx.view.RsccHomeView;
 import ch.imedias.rsccfx.view.RsccHomePresenter;
+import ch.imedias.rsccfx.view.RsccHomeView;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
