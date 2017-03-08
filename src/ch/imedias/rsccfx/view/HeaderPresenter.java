eff10675042ed4baf519c370a0112479759f184e
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-03-08 16:17:28 +0100

    Created methods for setting actions in the presenter. It can be used universally.

--- src/ch/imedias/rsccfx/view/HeaderPresenter.java
+++ src/ch/imedias/rsccfx/view/HeaderPresenter.java
@@ -1,6 +1,8 @@
 package ch.imedias.rsccfx.view;
 
 import ch.imedias.rsccfx.model.Rscc;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -22,11 +24,45 @@ public class HeaderPresenter {
   }
 
   private void attachEvents() {
-    view.backBtn.setOnAction(event -> showHomeView());
+    // view.backBtn.setOnAction(event -> showHomeView());
   }
 
-  private void showHomeView() {
+  /* private void showHomeView() {
     Stage stage = (Stage) view.getScene().getWindow();
     stage.setScene(new Scene(new RsccHomeView(model)));
+  } */
+
+  /**
+   *
+   * @param action is used to give the method a lambda expression.
+   */
+  public void setBackBtnAction(EventHandler<ActionEvent> action) {
+    view.backBtn.setOnAction(action);
   }
+
+  /**
+   *
+   * @param title is used to hand in a new string.
+   */
+  public void setHeaderTitle(String title) {
+    view.headLbl.textProperty().set(title);
+  }
+
+  /**
+   *
+   * @param action is used to give the method a lambda expression.
+   */
+  public void setHelpBtnAction(EventHandler<ActionEvent> action) {
+    view.helpBtn.setOnAction(action);
+  }
+
+  /**
+   *
+   * @param action is used to give the method a lambda expression.
+   */
+  public void setSettingsBtnAction(EventHandler<ActionEvent> action) {
+    view.settBtn.setOnAction(action);
+  }
+  
+  
 }
