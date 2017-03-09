48e62baac5ac19a24932ec51d4143bac98e260a9
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2017-03-09 11:41:11 +0100

    Added Screenloader to change between views.

--- src/ch/imedias/rsccfx/RsccApp.java
+++ src/ch/imedias/rsccfx/RsccApp.java
@@ -6,7 +6,9 @@ import ch.imedias.rsccfx.model.SystemCommander;
 import ch.imedias.rsccfx.view.RsccHomePresenter;
 import ch.imedias.rsccfx.view.RsccHomeView;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class RsccApp extends Application {
@@ -22,28 +24,37 @@ public class RsccApp extends Application {
     model = new Rscc(new SystemCommander());
     // RsccRequestHelpView showTokenView = new RsccRequestHelpView(model);
     // RsccSupporterView enterTokenView = new RsccSupporterView(model);
-    RsccHomeView view = new RsccHomeView(model);
+    ScreenLoader screenLoader = new ScreenLoader(model);
+    //RsccHomeView view = new RsccHomeView(model);
 
 
     // the scene to listen for the focus change
-    Scene scene = new Scene(view);
+    Scene scene = screenLoader.setScreens("homeView");
     String stSheet = getClass().getClassLoader().getResource("css/HomeStyle.css").toExternalForm();
     scene.getStylesheets().add(stSheet);
-    RsccHomePresenter presenter = new RsccHomePresenter(model, view);
+    RsccHomePresenter presenter = new RsccHomePresenter(model, view, screenLoader);
 
-    stage.setWidth(1000);
-    stage.setHeight(450);
-    stage.setMinWidth(250);
-    stage.setMinHeight(300);
     stage.setScene(scene);
 
-    view.initBtnPanel(scene);
+    //view.initBtnPanel(scene);
 
     stage.setTitle(APP_NAME);
-    stage.setHeight(400);
-    stage.setWidth(700);
-    stage.show();
 
+    // Get Screensize.
+
+    Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+    // Set Stage boundaries to visible bounds of the main screen.
+
+    stage.setWidth(primaryScreenBounds.getWidth() / 1.8);
+    stage.setHeight(primaryScreenBounds.getHeight() / 1.5);
+    stage.setX(primaryScreenBounds.getWidth() / 2 - stage.getWidth() / 2);
+    stage.setY(primaryScreenBounds.getHeight() / 2 - stage.getHeight() / 2);
+
+    stage.setMinWidth((primaryScreenBounds.getWidth() / 1.8) / 5);
+    stage.setMinHeight((primaryScreenBounds.getHeight() / 1.5) / 1.5);
+
+    stage.show();
   }
 
   @Override
