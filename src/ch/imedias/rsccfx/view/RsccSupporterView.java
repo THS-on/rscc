373edc7d9271f1ac38220b3978da96ed52d69e4f
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2017-03-16 15:33:54 +0100

    Merged conflicts.

--- src/ch/imedias/rsccfx/view/RsccSupporterView.java
+++ src/ch/imedias/rsccfx/view/RsccSupporterView.java
@@ -14,109 +14,111 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
-public class RsccSupporterView extends BorderPane implements View{
+
 /**
- * TODO: Javadoc comment here.
+ *
  */
-public class RsccSupporterView extends VBox {
+public class RsccSupporterView extends BorderPane implements View {
 
-  private final Rscc model;
+    private final Rscc model;
 
-  TitledPane adminSupporterPane;
-  TitledPane mainPane;
-  Label enterTokenlbl;
-  Label loremIpsumlbl;
-  Label examplelbl;
-  Label instructionlbl;
+    TitledPane adminSupporterPane;
+    TitledPane mainPane;
+    Label enterTokenlbl;
+    Label loremIpsumlbl;
+    Label examplelbl;
+    Label instructionlbl;
 
-  VBox groupingbox;
-  HBox tokenValidationbox;
-  TextField tokentxt;
-  ImageView isValidimg;
+    VBox groupingbox;
+    HBox tokenValidationbox;
+    TextField tokentxt;
+    ImageView isValidimg;
 
-  Button connectbtn;
-  Button expandOptionbtn;
+    Button connectbtn;
+    Button expandOptionbtn;
 
-  HeaderView headerView;
-  HeaderPresenter headerPresenter;
+    HeaderView headerView;
+    HeaderPresenter headerPresenter;
 
-  VBox topBox;
-  VBox centerBox;
+    VBox topBox;
+    VBox centerBox;
 
 
-  /**
-   * TODO: Javadoc comment here.
-   * This is the view for the supporter to enter the token.
-   * @param model
-   */
-  public RsccSupporterView(Rscc model) {
-    this.model = model;
+    /**
+     * TODO: Javadoc comment here.
+     * This is the view for the supporter to enter the token.
+     *
+     * @param model
+     */
+    public RsccSupporterView(Rscc model) {
+      this.model = model;
 
-    initFieldData();
-    layoutForm();
-    bindFieldsToModel();
+      initFieldData();
+      layoutForm();
+      bindFieldsToModel();
+    }
+
+    private void layoutForm() {
+      this.setPadding(new Insets(5, 25, 5, 25));
+
+
+      enterTokenlbl.setFont(new Font(25));
+
+      loremIpsumlbl.setWrapText(true);
+
+      tokenValidationbox = new HBox();
+      tokentxt = new TextField();
+      tokentxt.setFont(new Font(30));
+
+      isValidimg.setSmooth(true);
+      tokenValidationbox.getChildren().addAll(tokentxt, isValidimg);
+      tokenValidationbox.setSpacing(5);
+      tokenValidationbox.setHgrow(tokentxt, Priority.ALWAYS);
+      tokenValidationbox.setAlignment(Pos.CENTER_LEFT);
+
+      groupingbox = new VBox();
+      groupingbox.getChildren().addAll(tokenValidationbox, instructionlbl);
+
+      centerBox.getChildren().addAll(enterTokenlbl,
+              loremIpsumlbl,
+              examplelbl,
+              groupingbox,
+              connectbtn,
+              expandOptionbtn);
+
+      connectbtn.setFont(new Font(30));
+    }
+
+    private void initFieldData() {
+
+      adminSupporterPane = new TitledPane();
+      mainPane = new TitledPane();
+
+      headerView = new HeaderView(model);
+      headerPresenter = new HeaderPresenter(model, headerView);
+      topBox = new VBox();
+      centerBox = new VBox();
+
+      isValidimg = new ImageView(getClass()
+              .getClassLoader()
+              .getResource("dialog-error.png")
+              .toExternalForm());
+      enterTokenlbl = new Label("EnterToken");
+      loremIpsumlbl = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit");
+      examplelbl = new Label("Number of characters: 8\nexample: 666xx666");
+      instructionlbl = new Label("Instructions");
+      connectbtn = new Button("Connect");
+      expandOptionbtn = new Button("More");
+
+      setCenter(centerBox);
+      topBox.getChildren().add(headerView);
+      setTop(topBox);
+    }
+
+    private void bindFieldsToModel() {
+      //make the bindings to the model
+
+    }
+
   }
 
-  private void layoutForm() {
-    this.setPadding(new Insets(5, 25, 5, 25));
-
-
-    enterTokenlbl.setFont(new Font(25));
-
-    loremIpsumlbl.setWrapText(true);
-
-    tokenValidationbox = new HBox();
-    tokentxt = new TextField();
-    tokentxt.setFont(new Font(30));
-
-    isValidimg.setSmooth(true);
-    tokenValidationbox.getChildren().addAll(tokentxt, isValidimg);
-    tokenValidationbox.setSpacing(5);
-    tokenValidationbox.setHgrow(tokentxt, Priority.ALWAYS);
-    tokenValidationbox.setAlignment(Pos.CENTER_LEFT);
-
-    groupingbox = new VBox();
-    groupingbox.getChildren().addAll(tokenValidationbox, instructionlbl);
-
-    centerBox.getChildren().addAll(enterTokenlbl,
-        loremIpsumlbl,
-        examplelbl,
-        groupingbox,
-        connectbtn,
-        expandOptionbtn);
-
-    connectbtn.setFont(new Font(30));
-  }
-
-  private void initFieldData() {
-
-    adminSupporterPane = new TitledPane();
-    mainPane = new TitledPane();
-
-    headerView = new HeaderView(model);
-    headerPresenter = new HeaderPresenter(model, headerView);
-    topBox = new VBox();
-    centerBox = new VBox();
-
-    isValidimg = new ImageView(getClass()
-        .getClassLoader()
-        .getResource("dialog-error.png")
-        .toExternalForm());
-    enterTokenlbl = new Label("EnterToken");
-    loremIpsumlbl = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit");
-    examplelbl = new Label("Number of characters: 8\nexample: 666xx666");
-    instructionlbl = new Label("Instructions");
-    connectbtn = new Button("Connect");
-    expandOptionbtn = new Button("More");
-
-    setCenter(centerBox);
-    topBox.getChildren().add(headerView);
-    setTop(topBox);
-  }
-
-  private void bindFieldsToModel() {
-    //make the bindings to the model
-
-  }
-
-}
