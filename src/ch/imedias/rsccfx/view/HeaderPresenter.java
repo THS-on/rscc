e3eb3321b145f9809faf95b7e1257b5447fdc181
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-03-16 15:58:25 +0100

    HeaderPresenter JavaDoc and code formatting done

--- src/ch/imedias/rsccfx/view/HeaderPresenter.java
+++ src/ch/imedias/rsccfx/view/HeaderPresenter.java
@@ -5,38 +5,38 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 
+/**
+ * Presenter class of HeaderView. Defines the behaviour of interactions
+ * and initializes the size of the GUI components.
+ */
 public class HeaderPresenter {
-  private final Rscc model;
+  private final Rscc model;   // FIXME: François, do we need the model in this specific presenter?
   private HeaderView view;
 
   /**
-   * TODO: Javadoc comment here.
+   * Initializes a new HeaderPresenter with the according view.
+   *
+   * @param model the presentation model to coordinate views.
+   * @param view  the view which needs to be configured.
    */
   public HeaderPresenter(Rscc model, HeaderView view) {
     this.model = model;
     this.view = view;
-    // attachEvents();
   }
-
-  private void attachEvents() {
-    // view.backBtn.setOnAction(event -> showHomeView());
-  }
-
-  /* private void showHomeView() {
-    Stage stage = (Stage) view.getScene().getWindow();
-    stage.setScene(new Scene(new RsccHomeView(model)));
-  } */
 
   /**
-   * TODO: Javadoc comment here.
-   * @param action is used to give the method a lambda expression.
+   * Sets the behaviour of the back button in the HeaderView.
+   *
+   * @param action is used to give the method a lambda expression and defines what happens
+   *               when the back button is clicked.
    */
   public void setBackBtnAction(EventHandler<ActionEvent> action) {
     view.backBtn.setOnAction(action);
   }
 
   /**
-   * TODO: Javadoc comment here.
+   * Sets the title in the middle of the HeaderView.
+   *
    * @param title is used to hand in a new string.
    */
   public void setHeaderTitle(String title) {
@@ -44,33 +44,34 @@ public class HeaderPresenter {
   }
 
   /**
-   * TODO: Javadoc comment here.
-   * @param action is used to give the method a lambda expression.
+   * Sets the behaviour of the help button in the HeaderView.
+   *
+   * @param action is used to give the method a lambda expression and defines what happens
+   *               when the help button is clicked.
    */
   public void setHelpBtnAction(EventHandler<ActionEvent> action) {
     view.helpBtn.setOnAction(action);
   }
 
   /**
-   * TODO: Javadoc comment here.
-   * @param action is used to give the method a lambda expression.
+   * Sets the behaviour of the settings button in the HeaderView.
+   *
+   * @param action is used to give the method a lambda expression and defines what happens
+   *               when the settings button is clicked.
    */
   public void setSettingsBtnAction(EventHandler<ActionEvent> action) {
     view.settBtn.setOnAction(action);
   }
 
   /**
-   * TODO: Javadoc comment here.
-   * initSize method.
-   * */
+   * Initializes the size of the whole HeaderView elements.
+   *
+   * @param scene is needed to get the initial window size.
+   */
   public void initSize(Scene scene) {
-    // view.sep.prefWidthProperty().bind(scene.widthProperty());
     view.headLbl.prefWidthProperty().bind(scene.widthProperty());
     view.buttonBox.prefWidthProperty().bind(scene.widthProperty());
-    // view.separatorBox.prefWidthProperty().bind(scene.widthProperty());
     view.headerBox.prefWidthProperty().bind(scene.widthProperty());
-
-
   }
-  
+
 }
