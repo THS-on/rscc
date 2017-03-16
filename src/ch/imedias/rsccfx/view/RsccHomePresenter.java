92c838b8513178e921e02d7ca4a1226230a423b0
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-03-16 16:54:12 +0100

    prepared for easy merging later on. Also javadoc in all the files.

--- src/ch/imedias/rsccfx/view/RsccHomePresenter.java
+++ src/ch/imedias/rsccfx/view/RsccHomePresenter.java
@@ -1,42 +1,53 @@
 package ch.imedias.rsccfx.view;
 
-import ch.imedias.rsccfx.ScreenLoader;
+import ch.imedias.rsccfx.ControlledPresenter;
+import ch.imedias.rsccfx.ViewController;
 import ch.imedias.rsccfx.model.Rscc;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * TODO: Javadoc comment here.
+ * Presenter class of RsccHomeView. Defines the behaviour of interactions
+ * and initializes the size of the GUI components.
  */
-public class RsccHomePresenter {
+public class RsccHomePresenter implements ControlledPresenter {
   private final Rscc model;
-  private final RsccHomeView homeView;
-  private final ScreenLoader screenLoader;
-
+  private final RsccHomeView view;
+  private ViewController viewParent;
 
   /**
-   * TODO: Javadoc comment here.
+   * Initilizes a new RsccHomePresenter with the according view.
+   *
+   * @param model the presentation model to coordinate views.
+   * @param view  the view which needs to be configured.
    */
-  public RsccHomePresenter(Rscc model, RsccHomeView homeView, ScreenLoader screenLoader) {
+  public RsccHomePresenter(Rscc model, RsccHomeView view) {
     this.model = model;
-    this.homeView = homeView;
-    this.screenLoader = screenLoader;
+    this.view = view;
     attachEvents();
   }
 
-  private void attachEvents() {
+  /**
+   * Definies the ViewController to allow changing views.
+   *
+   * @param viewParent the controller to be used.
+   */
+  public void setViewParent(ViewController viewParent) {
+    this.viewParent = viewParent;
+  }
 
-    homeView.requestSupportBtn.setOnAction(event -> showRequestHelpView());
-    homeView.offerSupportBtn.setOnAction(event -> showSupporterView());
+  private void attachEvents() {
+    view.requestSupportBtn.setOnAction(event -> showRequestHelpView());
+    view.offerSupportBtn.setOnAction(event -> showSupporterView());
   }
 
   private void showSupporterView() {
-    Stage stage = (Stage) homeView.getScene().getWindow();
+    Stage stage = (Stage) view.getScene().getWindow();
     stage.setScene(new Scene(new RsccSupporterView(model)));
   }
 
   private void showRequestHelpView() {
-    Stage stage = (Stage) homeView.getScene().getWindow();
+    Stage stage = (Stage) view.getScene().getWindow();
     stage.setScene(new Scene(new RsccRequestHelpView(model)));
   }
 }
--- src/ch/imedias/rsccfx/view/RsccRequestHelpView.java
+++ src/ch/imedias/rsccfx/view/RsccRequestHelpView.java
@@ -4,14 +4,12 @@ package ch.imedias.rsccfx.view;
  * import statements.
  */
 
-import ch.imedias.rsccfx.ScreenLoader;
 import ch.imedias.rsccfx.model.Rscc;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
-import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
