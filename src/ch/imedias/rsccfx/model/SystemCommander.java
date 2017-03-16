58150416b49ec85fa9f24389a6ff6ee2b7af6e82
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-03-16 15:37:42 +0100

    systemcommander JavaDoc done

--- src/ch/imedias/rsccfx/model/SystemCommander.java
+++ src/ch/imedias/rsccfx/model/SystemCommander.java
@@ -7,9 +7,8 @@ public class SystemCommander {
 
   /**
    * Executes a command in the Linux terminal.
-   * more detailed instructions concerning this command can be found here:
+   * More detailed instructions concerning this command can be found here:
    * https://www.cs.technik.fhnw.ch/confluence16/pages/viewpage.action?pageId=6929334
-   *
    * @param command terminal command to be executed.
    * @return String trimmed output of the terminal without whitespaces at beginning / end.
    */
