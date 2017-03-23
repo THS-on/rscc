3c5e1c23ab8ab85f4498d179e30722401fcb1d1b
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2017-03-23 09:00:33 +0100

    Implemented Titledpane in RsccRequestView and modified expanded behavior.

--- src/ch/imedias/rsccfx/view/RsccRequestView.java
+++ src/ch/imedias/rsccfx/view/RsccRequestView.java
@@ -4,6 +4,7 @@ import ch.imedias.rsccfx.model.Rscc;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -28,9 +29,12 @@ public class RsccRequestView extends BorderPane {
   VBox mainBox = new VBox();
   VBox bottomBox = new VBox();
 
-  HBox expandableBox = new HBox();
+  HBox supporterAdminBox = new HBox();
   VBox centerBox = new VBox();
   HBox keyGeneratingBox = new HBox();
+
+  TitledPane keyGeneratorPane = new TitledPane();
+  TitledPane supporterAdminPane = new TitledPane();
 
   Text descriptionTxt = new Text();
   Text additionalDescriptionTxt = new Text();
@@ -91,6 +95,14 @@ public class RsccRequestView extends BorderPane {
         + "clita kasd gubergren, no sea takimata sanctus est "
         + "Lorem ipsum dolor sit amet.");
     additionalDescriptionTxt.setId("additionalDescriptionTxt");
+
+    // TODO: Implement String Class
+    keyGeneratorPane.setText("Key generator");
+    keyGeneratorPane.setExpanded(true);
+
+    supporterAdminPane.setText("Supporter Administration");
+    supporterAdminPane.setExpanded(false);
+
   }
 
   private void layoutForm() {
@@ -98,23 +110,29 @@ public class RsccRequestView extends BorderPane {
     centerBox.setId("centerBox");
     bottomBox.setId("bottomBox");
 
-    expandableBox.getChildren().addAll(supporterAdminBtn, supporterAdminLbl);
+    supporterAdminBox.getChildren().addAll(supporterAdminBtn, supporterAdminLbl);
     keyGeneratingBox.getChildren().addAll(generatedKeyFld, reloadKeyBtn);
+    keyGeneratingBox.getChildren().add(supporterAdminBox);
 
     topBox.getChildren().add(headerView);
     centerBox.getChildren().addAll(keyGenerationLbl, descriptionTxt, keyGeneratingBox,
         additionalDescriptionTxt);
-    bottomBox.getChildren().add(expandableBox);
+    bottomBox.getChildren().add(supporterAdminBox);
+
+    keyGeneratorPane.setContent(centerBox);
+    supporterAdminPane.setContent(bottomBox);
 
     setTop(topBox);
-    setCenter(centerBox);
-    setBottom(bottomBox);
+    setCenter(keyGeneratorPane);
+    setBottom(supporterAdminPane);
   }
 
   private void bindFieldsToModel() {
     // make bindings to the model
     generatedKeyFld.textProperty().bind(model.keyProperty());
   }
+
+
 }
 
 
