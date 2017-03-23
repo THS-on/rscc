b97e4dea0bb018eda3e6a92a5971948c31cd79bb
Jennifer Müller <jennifer.mueller1@students.fhnw.ch>
2017-03-23 17:23:54 +0100

    Resolved merge fail.

--- src/ch/imedias/rsccfx/view/RsccSupportPresenter.java
+++ src/ch/imedias/rsccfx/view/RsccSupportPresenter.java
@@ -83,14 +83,14 @@ public class RsccSupportPresenter implements ControlledPresenter {
 
     view.tokenTxt.setOnKeyPressed(event -> {
       view.isValidImg.setImage(new Image(validationImage(view.tokenTxt.getText())));
-      
+    });
+
     // Closes the other TitledPane so that just one TitledPane is shown on the screen.
     view.keyInputPane.setOnMouseClicked(event -> view.predefinedAdressesPane.setExpanded(false));
     view.predefinedAdressesPane.setOnMouseClicked(event -> view.keyInputPane.setExpanded(false));
-    });
-
-    // FIXME: Thank you.
-    /*view.connectBtn.setOnAction(
+  }
+  // FIXME: Thank you.
+  /*view.connectBtn.setOnAction(
         event -> {
           model.keyProperty().set(view.tokenTxt.getText());
           model.connectToUser(model.getKey(), FORWARDING_PORT, KEY_SERVER_IP,
@@ -99,7 +99,7 @@ public class RsccSupportPresenter implements ControlledPresenter {
     );*/
 
 
-   /**
+  /**
    * Initializes the functionality of the header, e.g. back and settings button.
    */
   private void initHeader() {
@@ -107,6 +107,6 @@ public class RsccSupportPresenter implements ControlledPresenter {
     headerPresenter = new HeaderPresenter(model, view.headerView);
     headerPresenter.setBackBtnAction(event -> viewParent.setView("home"));
     // TODO: Set actions on buttons (Help, Settings)
-   }
-  
+  }
+
 }
