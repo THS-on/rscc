6813f20dda37fdf4efdc3e6ab324540dd572675c
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-04-13 15:10:20 +0200

    changed to the according pictures and deleted unnecessary ones

--- src/ch/imedias/rsccfx/view/RsccHomeView.java
+++ src/ch/imedias/rsccfx/view/RsccHomeView.java
@@ -57,7 +57,7 @@ public class RsccHomeView extends BorderPane {
 
   private void layoutForm() {
     InputStream requestHelpImagePath = getClass().getClassLoader()
-        .getResourceAsStream("images/INeedHelp2.svg");
+        .getResourceAsStream("images/needHelp.svg");
     requestImg = new Image(requestHelpImagePath);
     requestImgView = new ImageView(requestImg);
     requestImgView.setPreserveRatio(true);
