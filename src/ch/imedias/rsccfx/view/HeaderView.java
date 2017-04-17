0f7f6c22c69d3974e4f7617128f1dfc533043c87
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-04-17 12:56:17 +0200

    added help PopOver

--- src/ch/imedias/rsccfx/view/HeaderView.java
+++ src/ch/imedias/rsccfx/view/HeaderView.java
@@ -56,6 +56,9 @@ public class HeaderView extends HBox {
   Label bitCurrentSettingsLbl = new Label();
 
   Slider compressionSldr;
+  Slider qualitySldr;
+
+  Label helpLbl = new Label();
 
   ToggleButton toggleBtn = new ToggleButton();
 
@@ -125,9 +128,6 @@ public class HeaderView extends HBox {
 
     this.setHeight(HEADER_HEIGHT);
 
-    // TODO: Move up
-
-
     // Settings PopOver
     compressionLbl.textProperty().set("Kompression");
     compressionLbl.setId("compressionLbl");
@@ -136,12 +136,16 @@ public class HeaderView extends HBox {
     compressionSldr.setId("compressionSldr");
 
     qualityLbl.textProperty().set("Qualität");
-    Slider qualitySldr = new Slider(0, 100, 10);
+    qualitySldr = new Slider(0, 100, 10);
+
     bitSettingsLbl.textProperty().set("8-Bit-Farben");
+    bitSettingsLbl.setId("bitSettingsLbl");
 
     toggleBtn.textProperty().set("On");
+    toggleBtn.setId("toggleBtn");
 
     bitCurrentSettingsLbl.textProperty().set("Ihre momentane Einstellung ist");
+    bitCurrentSettingsLbl.setId("bitCurrentSettingsLbl");
 
     settingsBox.getChildren().add(new HBox(compressionLbl, compressionSldr));
     settingsBox.getChildren().add(new HBox(qualityLbl, qualitySldr));
@@ -149,6 +153,16 @@ public class HeaderView extends HBox {
     settingsBox.getChildren().add(bitCurrentSettingsLbl);
 
     settingsPopOver.setArrowLocation(PopOver.ArrowLocation.TOP_RIGHT);
+
+    // Help popover
+    helpLbl.textProperty().set("The remote support tool allows you to get help " +
+        "or help someone in need");
+    helpLbl.setId("helpLbl");
+
+    // TODO: If we have more labels, we can add it to the box.
+    helpBox.getChildren().addAll(helpLbl);
+
+    helpPopOver.setArrowLocation(PopOver.ArrowLocation.TOP_RIGHT);
 
     // TODO: Use according method in "HeaderPresenter.java"!
     settingsBtn.setOnAction(event -> settingsPopOver.show(settingsBtn));
