f001986edac6940ec570e7bd7d37e0cf63f44072
jpduloch <roland.mosimann@students.fhnw.ch>
2017-04-20 23:40:11 +0200

    Updated comments and implemented setup()

--- test/ch/imedias/rsccfx/model/SystemCommanderTest.java
+++ test/ch/imedias/rsccfx/model/SystemCommanderTest.java
@@ -3,17 +3,30 @@ package ch.imedias.rsccfx.model;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Created by jp on 20/04/17.
+ * Tests the SystemCommander Class
  */
 public class SystemCommanderTest {
 
+  SystemCommander systemCommander;
+
+  /**
+   * Initializes test fixture before each test.
+   */
+  @Before
+  public void setUp() throws Exception {
+    systemCommander = new SystemCommander();
+  }
+
+  /**
+   * Test for {@link SystemCommander#executeTerminalCommand(String)}.
+   */
   @Test
   public void testExecuteTerminalCommandIllegalArguments() throws Exception {
     try {
-      SystemCommander systemCommander = new SystemCommander();
       systemCommander.executeTerminalCommand(null);
       fail("IllegalArgumentException was expected when Command is null");
     } catch (IllegalArgumentException e) {
@@ -21,12 +34,14 @@ public class SystemCommanderTest {
     }
   }
 
+  /**
+   * Test for {@link SystemCommander#executeTerminalCommand(String)}.
+   * Tests method with standard Linux shell-command "echo"
+   */
   @Test
   public void testExecuteTerminalCommand() throws Exception {
-    //Tests method with shellcommand "echo"
     String teststring = "testExecuteTerminalCommand";
     StringBuilder command = new StringBuilder("echo ").append(teststring);
-    SystemCommander systemCommander = new SystemCommander();
     String output = systemCommander.executeTerminalCommand(command.toString());
     assertEquals(teststring, output);
   }
