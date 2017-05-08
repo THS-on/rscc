40ed333d7217e370ff86a5a2668dbe7b2f0ccb00
martinfrancois <fracoisma@hotmail.com>
2017-05-08 10:04:00 +0200

    code reformat

--- src/ch/imedias/rsccfx/view/util/KeyTextField.java
+++ src/ch/imedias/rsccfx/view/util/KeyTextField.java
@@ -3,7 +3,7 @@ package ch.imedias.rsccfx.view.util;
 import javafx.scene.control.TextField;
 
 /**
- * Created by François Martin on 05.05.17.
+ *
  */
 public class KeyTextField extends TextField {
 
@@ -20,26 +20,21 @@ public class KeyTextField extends TextField {
   }
 
   @Override
-  public void replaceText(int start, int end, String text)
-  {
-    if (validate(text))
-    {
+  public void replaceText(int start, int end, String text) {
+    if (validate(text)) {
       super.replaceText(start, end, text);
     }
   }
 
   @Override
-  public void replaceSelection(String text)
-  {
-    if (validate(text))
-    {
+  public void replaceSelection(String text) {
+    if (validate(text)) {
       super.replaceSelection(text);
     }
   }
 
   // match for digits, spaces and backspaces
-  private boolean validate(String text)
-  {
+  private boolean validate(String text) {
     return text.matches("[0-9 ]*");
   }
 
