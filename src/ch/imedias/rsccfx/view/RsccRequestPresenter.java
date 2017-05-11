932a4bf8bb9b1ad3a372961da3aed0c59863428c
Jennifer Mueller <jennifer.mueller1@students.fhnw.ch>
2017-05-11 10:52:59 +0200

    Removed Comment.

--- src/ch/imedias/rsccfx/view/RsccRequestPresenter.java
+++ src/ch/imedias/rsccfx/view/RsccRequestPresenter.java
@@ -130,30 +130,29 @@ public class RsccRequestPresenter implements ControlledPresenter {
    */
   public void initSize(Scene scene) {
     // initialize header
-  /*
+
       // initialize view
       // TODO: requestHelpView --> generatedKeyFld should not take the whole width!
       view.generatedKeyFld.prefWidthProperty().bind(scene.widthProperty()
           .subtract(WIDTH_SUBTRACTION_KEYFIELD));
       view.descriptionLbl.prefWidthProperty().bind(scene.widthProperty()
           .subtract(WIDTH_SUBTRACTION_GENERAL));
-      view.keyGeneratorPane.prefWidthProperty().bind(scene.widthProperty());
-      view.keyGeneratorPane.maxWidthProperty().bind(scene.widthProperty());
+      view.keyGenerationInnerPane.prefWidthProperty().bind(scene.widthProperty());
+      view.keyGenerationInnerPane.maxWidthProperty().bind(scene.widthProperty());
 
       view.predefinedAddressesTitledPane.prefWidthProperty().bind(scene.widthProperty());
       view.predefinedAddressesTitledPane.maxWidthProperty().bind(scene.widthProperty());
 
       // FIXME: need the height of the titlePane itself... or magic number. François
-      view.centerBox.prefHeightProperty().bind(scene.heightProperty()
-          .subtract(195d));
 
-      view.keyGeneratorPane.prefWidthProperty().bind(scene.widthProperty());
+
+      view.keyGenerationInnerPane.prefWidthProperty().bind(scene.widthProperty());
 
       view.predefinedAdressesInnerBox.prefHeightProperty().bind(scene.heightProperty()
           .subtract(155d));
 
       view.supporterDescriptionLbl.prefWidthProperty().bind(scene.widthProperty().divide(3));
-      view.supporterGrid.prefWidthProperty().bind(scene.widthProperty().divide(3).multiply(2));*/
+      view.supporterGrid.prefWidthProperty().bind(scene.widthProperty().divide(3).multiply(2));
 
   }
 
