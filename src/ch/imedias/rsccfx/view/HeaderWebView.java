ee5c132ce16488d3de73394541927a7e64c72819
Gyssim <simon.gysin@students.fhnw.ch>
2017-05-11 15:59:18 +0200

    created local help page

--- src/ch/imedias/rsccfx/view/HeaderWebView.java
+++ src/ch/imedias/rsccfx/view/HeaderWebView.java
@@ -1,55 +1,27 @@
 package ch.imedias.rsccfx.view;
 
-import ch.imedias.rsccfx.RsccApp;
-import ch.imedias.rsccfx.localization.Strings;
-import ch.imedias.rsccfx.model.Rscc;
-import java.net.URL;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
-import javafx.concurrent.Worker.State;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javafx.geometry.Insets;
-import de.codecentric.centerdevice.javafxsvg.SvgImageLoaderFactory;
-import java.io.InputStream;
-import java.util.logging.Logger;
-import javafx.geometry.Pos;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
 
 /**
  * Class that created the help View
  */
 public class HeaderWebView extends VBox {
 
-  String wikiUrl="https://wiki.lernstick.ch";
+  final String wikiUrl="https://wiki.lernstick.ch";
+  final ProgressBar progressBar = new ProgressBar();
+  final WebView browser = new WebView();
+  final WebEngine webEngine = browser.getEngine();
+  final Worker<Void> worker = webEngine.getLoadWorker();
 
   public HeaderWebView() {
     createWebHelp();
   }
 
   public void createWebHelp() {
-
-    ProgressBar progressBar = new ProgressBar();
-
-    final WebView browser = new WebView();
-    final WebEngine webEngine = browser.getEngine();
-
-    // A Worker load the page
-    Worker<Void> worker = webEngine.getLoadWorker();
 
     // Bind the progress property of ProgressBar
     // with progress property of Worker
@@ -67,3 +39,4 @@ public class HeaderWebView extends VBox {
     webEngine.load(wikiUrl);
   }
 }
+
