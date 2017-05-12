039d0a7110d5572ed8e816dc9363f32f635ebd6d
Gyssim <simon.gysin@students.fhnw.ch>
2017-05-12 09:16:57 +0200

    refactoring

--- src/ch/imedias/rsccfx/view/HeaderWebView.java
+++ src/ch/imedias/rsccfx/view/HeaderWebView.java
@@ -1,21 +1,28 @@
 package ch.imedias.rsccfx.view;
 
+import ch.imedias.rsccfx.model.Rscc;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.concurrent.Worker;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
+
 /**
- * Class that created the help View
+ * Class that created the help View.
  */
 public class HeaderWebView extends VBox {
+  private static final Logger LOGGER =
+      Logger.getLogger(Rscc.class.getName());
 
-  final String wikiUrl="https://wiki.lernstick.ch";
+  final String wikiUrl = "https://wiki.lernstick.ch";
   final ProgressBar progressBar = new ProgressBar();
   final WebView browser = new WebView();
   final WebEngine webEngine = browser.getEngine();
   final Worker<Void> worker = webEngine.getLoadWorker();
+  final int BROWSERWIDTH = 1000;
 
   public HeaderWebView() {
     initWebHelp();
@@ -26,17 +33,24 @@ public class HeaderWebView extends VBox {
     // Bind the progress property of ProgressBar
     // with progress property of Worker
     progressBar.progressProperty().bind(worker.progressProperty());
-    progressBar.setPrefWidth(1000);
+    System.out.println(this.widthProperty().getValue());
+    progressBar.setPrefWidth(BROWSERWIDTH);
 
     this.getChildren().addAll(browser, progressBar);
 
-    String url1 = getClass().getClassLoader().getResource("helpPage.html").toExternalForm();
+    try  {
+      String url1 = getClass().getClassLoader().getResource("helpPage.html").toExternalForm();
+    } catch (NullPointerException e) {
+        LOGGER.log(Level.SEVERE, "File helpPage.html not found");
+    }
+
 
     //If local Help Page should be shown
     //webEngine.load(url1);
 
     //If external Wiki should be shown
     webEngine.load(wikiUrl);
+
   }
 }
 
