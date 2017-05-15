2f66ed7eac933a80e6c444107d25c86150f60c1e
jpduloch <roland.mosimann@students.fhnw.ch>
2017-05-15 17:27:48 +0200

    Rebuild VNCSErverHandler

---
+++ src/ch/imedias/rsccfx/localization/Strings.java
@@ -0,0 +1,191 @@
+package ch.imedias.rsccfx.localization;
+
+/**
+ *  Code generated at 17:27:06 15.05.2017
+ *  by generateStringsClass.py version 0.3.4
+ *  based on properties file with hash:
+ *  334b3d067fb0079e1043df17b6fdaacec028960a7838548b6fbedadad576a8b6
+ */
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Strings {
+
+  private ResourceBundle rscBndl;
+
+  public String address;
+  public String connectedTo;
+  public String connectingTo;
+  public String connectionFailed;
+  public String description;
+  public String editDialogCancelButtonText;
+  public String editDialogDefaultButtonToolTipText;
+  public String editDialogDeleteButtonToolTipText;
+  public String editDialogDownButtonToolTipText;
+  public String editDialogInsertButtonToolTipText;
+  public String editDialogOkButtonText;
+  public String editDialogTitle;
+  public String editDialogUpButtonToolTipText;
+  public String encryption;
+  public String error;
+  public String errorNoPort;
+  public String remoteSupportDialogCancelButtonText;
+  public String remoteSupportDialogConnectButtonText;
+  public String remoteSupportDialogSupportAddressLabelText;
+  public String remoteSupportDialogTitle;
+  public String remoteSupportFrameBgr233CheckBoxText;
+  public String remoteSupportFrameCompressionLabelText;
+  public String remoteSupportFrameDisconnectButtonText;
+  public String remoteSupportFrameEditButtonToolTipText;
+  public String remoteSupportFrameOfferSupportPanelTabConstraintsTabTitle;
+  public String remoteSupportFramePicturePropertiesPanelBorderTitle;
+  public String remoteSupportFrameQualityLabelText;
+  public String remoteSupportFrameScaleLabelText;
+  public String remoteSupportFrameSecurePortsLabelText;
+  public String remoteSupportFrameSecurePortsTextFieldToolTipText;
+  public String remoteSupportFrameSeekSupportPanelTabConstraintsTabTitle;
+  public String remoteSupportFrameTitle;
+  public String startService;
+  public String stopService;
+  public String homeRequestBigLbl;
+  public String homeRequestSmallLbl;
+  public String homeSupportBigLbl;
+  public String homeSupportSmallLbl;
+  public String requestTitleLbl;
+  public String requestDescriptionLbl;
+  public String requestKeyGeneratorPane;
+  public String requestGeneratedKeyFld;
+  public String requestPredefinedAdressessPane;
+  public String requestStatusLblWaiting;
+  public String requestStatusLblReady;
+  public String supportTitleLbl;
+  public String supportDescriptionLbl;
+  public String supportConnectBtn;
+  public String supportKeyInputPane;
+  public String supportAdressBookPane;
+  public String supportStatusLblWaiting;
+  public String supportStatusLblReady;
+  public String requestSupporterDescriptionLbl;
+  public String startServiceDescpriptionLbl;
+  public String statusBoxServiceIdle;
+  public String statusBoxServiceStarted;
+  public String statusBoxServiceStopped;
+  public String supportHelpLbl;
+  public String requestHelpLbl;
+  public String homeHelpLbl;
+  public String dialogTitleText;
+  public String dialogNameText;
+  public String dialogAddressText;
+  public String dialogPortText;
+  public String dialogImageText;
+  public String dialogChargeableLbl;
+  public String dialogEncryptedLbl;
+  public String supportCompressionLbl;
+  public String supportQualityLbl;
+  public String supportBgr233Lbl;
+  public String requestViewOnlyLbl;
+  public String bgr233DescriptionLbl;
+  public String expertSettingsDialogTitle;
+  public String expertForceConnectOverServerLbl;
+  public String expertKeyserverIpLbl;
+  public String expertKeyserverHttpLbl;
+  public String expertVncPortLbl;
+  public String expertIcePortLbl;
+  public String expertUdpPackageSizeLbl;
+  public String expertForwardingPortLbl;
+  public String expertStunServerPortLbl;
+  public String expertStunserverLbl;
+  public String expertSettingsBtn;
+  public String settingsTitleLbl;
+
+  public Strings() {
+
+    rscBndl = ResourceBundle.getBundle("ch/imedias/rsccfx/localization/Bundle");
+
+    address = rscBndl.getString("address");
+    connectedTo = rscBndl.getString("connectedTo");
+    connectingTo = rscBndl.getString("connectingTo");
+    connectionFailed = rscBndl.getString("connectionFailed");
+    description = rscBndl.getString("description");
+    editDialogCancelButtonText = rscBndl.getString("editDialogCancelButtonText");
+    editDialogDefaultButtonToolTipText = rscBndl.getString("editDialogDefaultButtonToolTipText");
+    editDialogDeleteButtonToolTipText = rscBndl.getString("editDialogDeleteButtonToolTipText");
+    editDialogDownButtonToolTipText = rscBndl.getString("editDialogDownButtonToolTipText");
+    editDialogInsertButtonToolTipText = rscBndl.getString("editDialogInsertButtonToolTipText");
+    editDialogOkButtonText = rscBndl.getString("editDialogOkButtonText");
+    editDialogTitle = rscBndl.getString("editDialogTitle");
+    editDialogUpButtonToolTipText = rscBndl.getString("editDialogUpButtonToolTipText");
+    encryption = rscBndl.getString("encryption");
+    error = rscBndl.getString("error");
+    errorNoPort = rscBndl.getString("errorNoPort");
+    remoteSupportDialogCancelButtonText = rscBndl.getString("remoteSupportDialogCancelButtonText");
+    remoteSupportDialogConnectButtonText = rscBndl.getString("remoteSupportDialogConnectButtonText");
+    remoteSupportDialogSupportAddressLabelText = rscBndl.getString("remoteSupportDialogSupportAddressLabelText");
+    remoteSupportDialogTitle = rscBndl.getString("remoteSupportDialogTitle");
+    remoteSupportFrameBgr233CheckBoxText = rscBndl.getString("remoteSupportFrameBgr233CheckBoxText");
+    remoteSupportFrameCompressionLabelText = rscBndl.getString("remoteSupportFrameCompressionLabelText");
+    remoteSupportFrameDisconnectButtonText = rscBndl.getString("remoteSupportFrameDisconnectButtonText");
+    remoteSupportFrameEditButtonToolTipText = rscBndl.getString("remoteSupportFrameEditButtonToolTipText");
+    remoteSupportFrameOfferSupportPanelTabConstraintsTabTitle = rscBndl.getString("remoteSupportFrameOfferSupportPanelTabConstraintsTabTitle");
+    remoteSupportFramePicturePropertiesPanelBorderTitle = rscBndl.getString("remoteSupportFramePicturePropertiesPanelBorderTitle");
+    remoteSupportFrameQualityLabelText = rscBndl.getString("remoteSupportFrameQualityLabelText");
+    remoteSupportFrameScaleLabelText = rscBndl.getString("remoteSupportFrameScaleLabelText");
+    remoteSupportFrameSecurePortsLabelText = rscBndl.getString("remoteSupportFrameSecurePortsLabelText");
+    remoteSupportFrameSecurePortsTextFieldToolTipText = rscBndl.getString("remoteSupportFrameSecurePortsTextFieldToolTipText");
+    remoteSupportFrameSeekSupportPanelTabConstraintsTabTitle = rscBndl.getString("remoteSupportFrameSeekSupportPanelTabConstraintsTabTitle");
+    remoteSupportFrameTitle = rscBndl.getString("remoteSupportFrameTitle");
+    startService = rscBndl.getString("startService");
+    stopService = rscBndl.getString("stopService");
+    homeRequestBigLbl = rscBndl.getString("homeRequestBigLbl");
+    homeRequestSmallLbl = rscBndl.getString("homeRequestSmallLbl");
+    homeSupportBigLbl = rscBndl.getString("homeSupportBigLbl");
+    homeSupportSmallLbl = rscBndl.getString("homeSupportSmallLbl");
+    requestTitleLbl = rscBndl.getString("requestTitleLbl");
+    requestDescriptionLbl = rscBndl.getString("requestDescriptionLbl");
+    requestKeyGeneratorPane = rscBndl.getString("requestKeyGeneratorPane");
+    requestGeneratedKeyFld = rscBndl.getString("requestGeneratedKeyFld");
+    requestPredefinedAdressessPane = rscBndl.getString("requestPredefinedAdressessPane");
+    requestStatusLblWaiting = rscBndl.getString("requestStatusLblWaiting");
+    requestStatusLblReady = rscBndl.getString("requestStatusLblReady");
+    supportTitleLbl = rscBndl.getString("supportTitleLbl");
+    supportDescriptionLbl = rscBndl.getString("supportDescriptionLbl");
+    supportConnectBtn = rscBndl.getString("supportConnectBtn");
+    supportKeyInputPane = rscBndl.getString("supportKeyInputPane");
+    supportAdressBookPane = rscBndl.getString("supportAdressBookPane");
+    supportStatusLblWaiting = rscBndl.getString("supportStatusLblWaiting");
+    supportStatusLblReady = rscBndl.getString("supportStatusLblReady");
+    requestSupporterDescriptionLbl = rscBndl.getString("requestSupporterDescriptionLbl");
+    startServiceDescpriptionLbl = rscBndl.getString("startServiceDescpriptionLbl");
+    statusBoxServiceIdle = rscBndl.getString("statusBoxServiceIdle");
+    statusBoxServiceStarted = rscBndl.getString("statusBoxServiceStarted");
+    statusBoxServiceStopped = rscBndl.getString("statusBoxServiceStopped");
+    supportHelpLbl = rscBndl.getString("supportHelpLbl");
+    requestHelpLbl = rscBndl.getString("requestHelpLbl");
+    homeHelpLbl = rscBndl.getString("homeHelpLbl");
+    dialogTitleText = rscBndl.getString("dialogTitleText");
+    dialogNameText = rscBndl.getString("dialogNameText");
+    dialogAddressText = rscBndl.getString("dialogAddressText");
+    dialogPortText = rscBndl.getString("dialogPortText");
+    dialogImageText = rscBndl.getString("dialogImageText");
+    dialogChargeableLbl = rscBndl.getString("dialogChargeableLbl");
+    dialogEncryptedLbl = rscBndl.getString("dialogEncryptedLbl");
+    supportCompressionLbl = rscBndl.getString("supportCompressionLbl");
+    supportQualityLbl = rscBndl.getString("supportQualityLbl");
+    supportBgr233Lbl = rscBndl.getString("supportBgr233Lbl");
+    requestViewOnlyLbl = rscBndl.getString("requestViewOnlyLbl");
+    bgr233DescriptionLbl = rscBndl.getString("bgr233DescriptionLbl");
+    expertSettingsDialogTitle = rscBndl.getString("expertSettingsDialogTitle");
+    expertForceConnectOverServerLbl = rscBndl.getString("expertForceConnectOverServerLbl");
+    expertKeyserverIpLbl = rscBndl.getString("expertKeyserverIpLbl");
+    expertKeyserverHttpLbl = rscBndl.getString("expertKeyserverHttpLbl");
+    expertVncPortLbl = rscBndl.getString("expertVncPortLbl");
+    expertIcePortLbl = rscBndl.getString("expertIcePortLbl");
+    expertUdpPackageSizeLbl = rscBndl.getString("expertUdpPackageSizeLbl");
+    expertForwardingPortLbl = rscBndl.getString("expertForwardingPortLbl");
+    expertStunServerPortLbl = rscBndl.getString("expertStunServerPortLbl");
+    expertStunserverLbl = rscBndl.getString("expertStunserverLbl");
+    expertSettingsBtn = rscBndl.getString("expertSettingsBtn");
+    settingsTitleLbl = rscBndl.getString("settingsTitleLbl");
+  }
+}
