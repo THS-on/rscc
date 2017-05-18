65208df95870a83aa2099c3e5e866a5547fb1ac9
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-05-18 17:55:53 +0200

    fixed checkstyle errors. also #1000 Jenkins build! :O

--- src/ch/imedias/rsccfx/view/util/NumberTextField.java
+++ src/ch/imedias/rsccfx/view/util/NumberTextField.java
@@ -1,45 +1,44 @@
 package ch.imedias.rsccfx.view.util;
 
-import javafx.scene.control.TextField;
-
 import java.util.function.Predicate;
+import javafx.scene.control.TextField;
 
 public class NumberTextField extends TextField {
 
-    /**
-     * Returns true if the entered text is "", which means that the backspace key was pressed.
-     */
-    private static final Predicate<String> IS_BACKSPACE = ""::equals;
+  /**
+   * Returns true if the entered text is "", which means that the backspace key was pressed.
+   */
+  private static final Predicate<String> IS_BACKSPACE = ""::equals;
 
-    /**
-     * Replaces a range of characters with the given text.
-     * Validation was added to the standard implementation.
-     */
-    @Override
-    public void replaceText(int start, int end, String text) {
-      if (validate(text)) {
-        super.replaceText(start, end, text);
-      }
+  /**
+   * Replaces a range of characters with the given text.
+   * Validation was added to the standard implementation.
+   */
+  @Override
+  public void replaceText(int start, int end, String text) {
+    if (validate(text)) {
+      super.replaceText(start, end, text);
     }
-
-    /**
-     * Replaces a range of characters with the given text upon selecting.
-     * Validation was added to the standard implementation.
-     */
-    @Override
-    public void replaceSelection(String text) {
-      if (validate(text)) {
-        super.replaceSelection(text);
-      }
-    }
-
-    /**
-     * Validates every input to make sure nothing but spaces, digits and backspaces can be entered.
-     */
-    private boolean validate(String text) {
-      return text.matches("[0-9]*")
-          || IS_BACKSPACE.test(text);
-    }
-
   }
+
+  /**
+   * Replaces a range of characters with the given text upon selecting.
+   * Validation was added to the standard implementation.
+   */
+  @Override
+  public void replaceSelection(String text) {
+    if (validate(text)) {
+      super.replaceSelection(text);
+    }
+  }
+
+  /**
+   * Validates every input to make sure nothing but spaces, digits and backspaces can be entered.
+   */
+  private boolean validate(String text) {
+    return text.matches("[0-9]*")
+        || IS_BACKSPACE.test(text);
+  }
+
+}
 
